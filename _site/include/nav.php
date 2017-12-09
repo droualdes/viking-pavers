bdb98bb7c1dfdcf0ad2d908679043c8680637220
David Roualdes <droualdes@me.com>
2017-12-08 18:19:10 -0800

    Ancient Site

---
+++ _site/include/nav.php
@@ -0,0 +1,144 @@
+<ul id="nav">
+	<li id="nav-1">
+		<a href="/" title="Home" class="current"><span>Home</span></a>
+	</li>
+	<li id="nav-2">
+		<a href="#" title="Services"><span>Services</span></a>
+		<ul>
+			<li>
+				<a href="/services/driveways/" title="Driveways">Driveways</a>
+			</li>
+			<li>
+				<a href="/services/firepits/" title="Fire Pits">Fire Pits</a>
+			</li>
+			<li>
+				<a href="/services/design/" title="Landscape Design">Landscape Design</a>
+			</li>
+			<li>
+				<a href="/services/lighting/" title="Landscape Lighting">Landscape Lighting</a>
+			</li>
+			<li>
+				<a href="/services/kitchens/" title="Outdoor Kitchens &amp; BBQ Islands">Outdoor Kitchens &amp; BBQ Islands</a>
+			</li>
+			<li>
+				<a href="/services/patios/" title="Patios">Patios</a>
+			</li>
+			<li>
+				<a href="/services/pooldecks/" title="Pool Decks">Pool Decks</a>
+			</li>
+			<li>
+				<a href="/services/walls/" title="Walls">Walls</a>
+			</li>
+			<li>
+				<a href="/services/steps/" title="Steps">Steps</a>
+			</li>
+			<li>
+				<a href="/services/turf/" title="Synthetic Turf Lawns">Synthetic Turf</a>
+			</li>
+			<li>
+				<a href="/services/walkways/" title="Walkways">Walkways</a>
+			</li>
+			<li>
+				<a href="/services/ancillary/" title="Demolition, Excavation, &amp; Removal">Demolition, Excavation, &amp; Removal</a>
+			</li>
+		</ul>
+	</li>
+	<li id="nav-3">
+		<a href="#" title="Products"><span>Products</span></a>
+		<ul>
+			<li>
+				<a href="/products/belgard/pavers/" title="Belgard Pavers available from Viking Pavers">Belgard Pavers</a>
+			</li>
+			<li>
+				<a href="/products/belgard/bullnose/" title="Belgard Bullnose available from Viking Pavers">Belgard Bullnose</a>
+			</li>
+			<li>
+				<a href="/products/belgard/walls/" title="Belgard Wall, Step, &amp; Fire Pit Blocks available from Viking Pavers">Belgard Wall, Step, &amp; Fire Pit Blocks</a>
+			</li>
+			<li>
+				<a href="/products/mcnear/pavers/" title="McNear Pavers available from Viking Pavers">McNear Pavers</a>
+			</li>
+			<li>
+				<a href="/products/mcnear/bullnose/" title="McNear Bullnose available from Viking Pavers">McNear Bullnose</a>
+			</li>
+			<li>
+				<a href="/products/mcnear/walls/" title="McNear Wall, Step, &amp; Fire Pit Blocks available from Viking Pavers">McNear Wall, Step, &amp; Fire Pit Blocks</a>
+			</li>
+			<li>
+				<a href="/products/calstone/pavers/" title="Calstone Pavers available from Viking Pavers">Calstone Pavers</a>
+			</li>
+			<li>
+				<a href="/products/calstone/bullnose/" title="Calstone Bullnose available from Viking Pavers">Calstone Bullnose</a>
+			</li>
+			<li>
+				<a href="/products/calstone/walls/" title="Calstone Wall, Step, &amp; Fire Pit Blocks available from Viking Pavers">Calstone Wall, Step, &amp; Fire Pit Blocks</a>
+			</li>
+			<li>
+				<a href="/products/johndeere/" title="John Deere Synthetic Turf available from Viking Pavers">John Deere Synthetic Turf</a>
+			</li>
+			<li>
+				<a href="/products/paver-lights/" title="Solar Powered Paver Lights available from Viking Pavers">Solar Powered Paver Lights</a>
+			</li>
+		</ul>
+	</li>
+	<li id="nav-4">
+		<a href="#" title="Superiority"><span>Superiority</span></a>
+		<ul>
+			<li>
+				<a href="/comparisons/why-use-us/" title="Why Use Us?">Why Use Us?</a>
+			</li>
+			<li>
+				<a href="http://youtu.be/Ts4HZQQFJu0?rel=0" target="_blank" title="Viking Pavers' Intro Video">Diamond Certified Video Profile</a>
+			</li>
+			<li>
+				<a href="/promotions/" title="Viking Pavers Promotions">Promotions</a>
+			</li>
+			<li>
+				<a href="/rebates/" title="Viking Pavers Rebates">Rebates</a>
+			</li>
+			<li>
+				<a href="/paymentoptions/" title="Viking Pavers Payment Options">Payment Options</a>
+			</li>
+			<li>
+				<a href="/warranty/" title="Viking Pavers Warranty">Materials &amp; Installation Warranty</a>
+			</li>
+			<li>
+				<a href="/qualityspecifications/" title="Viking Pavers Installation Quality Specifications">Installation Quality Specifications</a>
+			</li>
+		</ul>
+	</li>
+	<li id="nav-5">
+		<a href="#" title="Facts"><span>Facts</span></a>
+		<ul>
+			<li>
+				<a href="/comparisons/pavers-vs-concrete/" title="Pavers vs. Other Materials">Pavers vs. Other Materials</a>
+			</li>
+			<li>
+				<a href="/comparisons/synturf-vs-grass/" title="Synthetic Turf vs. Natural Grass">Synthetic Turf vs. Natural Grass</a>
+			</li>
+			<li>
+				<a href="/eco-facts/" title="Traditional Lawns &amp; the Environment">Traditional Lawns &amp; the Environment</a>
+			</li>
+			<li>
+				<a href="/roi/landscaping/" title="Landscaping ROI">Landscaping ROI</a>
+			</li>
+		</ul>
+	</li>
+	<li id="nav-6">
+		<a href="#" title="Proof of Quality"><span>Proof of Quality</span></a>
+		<ul>
+			<li>
+				<a href="/photogallery/" title="Viking Pavers Installations Photo Gallery">Our Photo Gallery</a>
+			</li>
+			<li>
+				<a href="/customercomments/" title="Viking Pavers Customer Comments">Customer Comments</a>
+			</li>
+			<li>
+				<a href="/thankyouletters/" title="Viking Pavers Thank You Letters">Thank You Letters</a>
+			</li>
+			<li>
+				<a href="/reviews/" title="Independent Reviews">Independent Reviews</a>
+			</li>
+		</ul>
+	</li>
+</ul><!-- nav -->
